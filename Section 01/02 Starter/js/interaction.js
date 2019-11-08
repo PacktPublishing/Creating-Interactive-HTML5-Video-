6b962ee5ff874ca531abb9ef669598709baf3cdb
packtbipin2377 <44016603+packtbipin2377@users.noreply.github.com>
2019-11-08 16:58:11 +0530

    Code Added
    Code Added

---
+++ Section 01/02 Starter/js/interaction.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+	// Code when DOM is ready goes here
+
+});
+
+
+/* DO NOT DELETE: Converting the time into minutes and seconds */
+function secondsToHms(d) {
+	d = Number(d);
+	var h = Math.floor(d / 3600);
+	var m = Math.floor(d % 3600 / 60);
+	var s = Math.floor(d % 3600 % 60);
+	return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s); 
+}
